ce15dc42201dea02130d991c97ec39e363506181
Standard-B <wjdtjr030@naver.com>
2023-07-17 16:37:43 +0900

    feat(호텔상세보기): 상세보기 페이지 추가
    객실 정보 추가
    주변 숙소 추가

--- src/main/java/org/hotelfinder/controller/MainController.java
+++ src/main/java/org/hotelfinder/controller/MainController.java
@@ -6,16 +6,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
 
-    @GetMapping("/index.do")
+    @GetMapping("/")
     public String index() {
 
         return "index";
     }
 
-    @GetMapping("/hotel/list.do")
+    @GetMapping("/hotels")
     public String hotelList() {
 
         return "hotels/hotel-list";
+    }
+
+    @GetMapping("/hotels/{hotel_id}")
+    public String hotelDetails() {
+
+        return "hotels/hotel-details";
     }
 
 }
